f99171610c623d7702a9355d6041e86c63f811c6
Gekolord <80892021+Gekolord@users.noreply.github.com>
2024-10-12 14:51:29 +0300

    almost finish working on add button

--- script.js
+++ script.js
@@ -44,44 +44,66 @@ let nameInput = document.querySelector(".form__element-name")
 let vacancyInput = document.querySelector(".form__element-vacancy")
 let phoneInput = document.querySelector(".form__element-phone")
 let addButton = document.querySelector(".form__element-add")
+let existingContacts = new Set();
 
-
-
-function addPerson() {
+// adds person to contacts and returns first letter of their name
+function addPersonToContacts() {
     let person = Object.fromEntries([
         ['name', nameInput.value],
         ['vacancy', vacancyInput.value],
         ['phone', phoneInput.value]
     ])
+    if (existingContacts.has(person.name)) {
+        person = null
+        return;
+    }
+
     allContacts[nameInput.value[0].toLowerCase()].push(person)
+    existingContacts.add(person.name)
     console.log(allContacts)
+    return person.name[0].toLowerCase()
 }
 
-addButton.addEventListener("click", addPerson)
-// let p = document.createElement("p")
-// p.innerText = `${allContacts.a[0].name} ${allContacts.a[0].vacancy} ${allContacts.a[0].phone}`
 
-// function showContacts(event) {
-    
-//     let div = event.target.closest("div");
-//     if (!div) return;
-//     if (!div.classList.contains("element__letter")) return;
-//     let p = document.createElement('p');
-//     let letter = div.dataset.id
-    
-//     allContacts[letter].forEach(element => {
-//         let newDiv = document.createElement('div')
-//         newDiv.append(`Name: ${element.name}\n`)
-//         newDiv.append(`Vacancy: ${element.vacancy}\n`)
-//         newDiv.append(`Phone: ${element.phone}\n\n`)
-//         div.append(newDiv)
-//     });
 
-//     // console.log(letter)
-    
-// }
+// Create a div with a class
+function createDiv(className) {
+    const div = document.createElement('div')
+    div.classList.add(className)
+    return div;
+}
+// adds inner text to existing div
+function renderContact(div, key, index) {
+    div.innerText = `Name: ${allContacts[key][index].name}\n
+                     Vacancy:${allContacts[key][index].vacancy}\n
+                     Phone: ${allContacts[key][index].phone}\n`
+}
 
-// contactTable.addEventListener("click", showContacts)
+
+
+addButton.addEventListener("click", addPersonToContacts)
+
+function showContacts(event) {
+    
+    let div = event.target.closest("div");
+    if (!div) return;
+    if (!div.classList.contains("element__letter")) return;
+    let p = document.createElement('p');
+    let letter = div.dataset.id
+    
+    allContacts[letter].forEach(element => {
+        let newDiv = document.createElement('div')
+        newDiv.append(`Name: ${element.name}\n`)
+        newDiv.append(`Vacancy: ${element.vacancy}\n`)
+        newDiv.append(`Phone: ${element.phone}\n\n`)
+        div.append(newDiv)
+    });
+
+    // console.log(letter)
+    
+}
+
+// contactTable.addEventListener("click", addPeopleToPage)
 
 // column__element-data-info
 // column__element-data-info_active
